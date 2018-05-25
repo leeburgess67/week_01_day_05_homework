f1a452b58fde80229aa20a510aa577d93567dd8f
leeburgess67 <leeburgessmail@gmail.com>
2018-05-25 11:53:55 +0100

    A few more tests passed

--- pet_shop_spec.rb
+++ pet_shop_spec.rb
@@ -76,24 +76,24 @@ class TestPetShop < MiniTest::Test
   # def test_pet_shop_name
   #   name = pet_shop_name(@pet_shop)
   #   assert_equal("Camelot of Pets", name)
+  # # end
+  #
+  # def test_total_cash
+  #   sum = total_cash(@pet_shop)
+  #   assert_equal(1000, sum)
   # end
-
-  def test_total_cash
-    sum = total_cash(@pet_shop)
-    assert_equal(1000, sum)
-  end
-
+  #
   # def test_add_or_remove_cash__add
   #   add_or_remove_cash(@pet_shop,10)
   #   cash = total_cash(@pet_shop)
   #   assert_equal(1010, cash)
   # end
 
-  # def test_add_or_remove_cash__remove
-  #   add_or_remove_cash(@pet_shop,-10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(990, cash)
-  # end
+  def test_add_or_remove_cash__remove
+    add_or_remove_cash(@pet_shop,-10)
+    cash = total_cash(@pet_shop)
+    assert_equal(990, cash)
+  end
 
   # def test_pets_sold
   #   sold = pets_sold(@pet_shop)
