86ea204f1a3d05869376754633d3b09a13641bea
leeburgess67 <leeburgessmail@gmail.com>
2018-05-25 21:50:10 +0100

    10 pssing tests

--- pet_shop_spec.rb
+++ pet_shop_spec.rb
@@ -72,64 +72,67 @@ class TestPetShop < MiniTest::Test
         name: "Camelot of Pets"
       }
   end
-  #
-  # def test_pet_shop_name
-  #   name = pet_shop_name(@pet_shop)
-  #   assert_equal("Camelot of Pets", name)
-  # # end
-  #
-  # def test_total_cash
-  #   sum = total_cash(@pet_shop)
-  #   assert_equal(1000, sum)
-  # end
-  #
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(1010, cash)
-  # end
-
+#1
+  def test_pet_shop_name
+    name = pet_shop_name(@pet_shop)
+    assert_equal("Camelot of Pets", name)
+  end
+#2
+  def test_total_cash
+    sum = total_cash(@pet_shop)
+    assert_equal(1000, sum)
+  end
+#3
+  def test_add_or_remove_cash__add
+    add_or_remove_cash(@pet_shop,10)
+    cash = total_cash(@pet_shop)
+    assert_equal(1010, cash)
+  end
+#4
   def test_add_or_remove_cash__remove
     add_or_remove_cash(@pet_shop,-10)
     cash = total_cash(@pet_shop)
     assert_equal(990, cash)
   end
+#5
+  def test_pets_sold
+    sold = pets_sold(@pet_shop)
+    assert_equal(0, sold)
+  end
+#6
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
+  #7
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
+#8
+  def test_all_pets_by_breed__found
+    pets = pets_by_breed(@pet_shop,
+     "British Shorthair")
+    assert_equal(2, pets.count)
+  end
+#9
+  def test_all_pets_by_breed__not_found
+    pets = pets_by_breed(@pet_shop, "Dalmation")
+    assert_equal(0, pets.count)
+    #needs to return an array
+  end
+#10
+  def test_find_pet_by_name__returns_pet
+    pet = find_pet_by_name(@pet_shop, "Arthur")
+    assert_equal("Arthur", pet[:name])
+  end
+#11
+  def test_find_pet_by_name__returns_nil
+    pet = find_pet_by_name(@pet_shop, "Fred")
+    assert_nil(pet)
+  end
 
-  # def test_pets_sold
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(0, sold)
-  # end
-
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
-
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
-
-  # def test_all_pets_by_breed__found
-  #   pets = pets_by_breed(@pet_shop, "British Shorthair")
-  #   assert_equal(2, pets.count)
-  # end
-
-  # def test_all_pets_by_breed__not_found
-  #   pets = pets_by_breed(@pet_shop, "Dalmation")
-  #   assert_equal(0, pets.count)
-  # end
-
-  # def test_find_pet_by_name__returns_pet
-  #   pet = find_pet_by_name(@pet_shop, "Arthur")
-  #   assert_equal("Arthur", pet[:name])
-  # end
-
-  # def test_find_pet_by_name__returns_nil
-  #   pet = find_pet_by_name(@pet_shop, "Fred")
-  #   assert_nil(pet)
-  # end
 
   # def test_remove_pet_by_name
   #   remove_pet_by_name(@pet_shop, "Arthur")
@@ -144,7 +147,7 @@ class TestPetShop < MiniTest::Test
   # end
 
   # def test_customer_cash
-  #   cash = customer_cash(customer[0])
+  #   cash = customer_cash(@customers[0])
   #   assert_equal(1000, cash)
   # end
 
